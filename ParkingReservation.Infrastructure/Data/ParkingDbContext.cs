2960b0ec835c41bd63a0641d53f18a138075d944
Patrick B. <barth.patrick-laszlo.23@stud.umfst.ro>
2026-08-11 17:54:54 +0300

    Add ParkingDbContext with entity configuration

---
+++ ParkingReservation.Infrastructure/Data/ParkingDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkingReservation.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingReservation.Infrastructure.Data;
+
+public class ParkingDbContext : DbContext
+{
+    public ParkingDbContext(DbContextOptions<ParkingDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<ParkingSpot> ParkingSpots => Set<ParkingSpot>();
+    public DbSet<Reservation> Reservations => Set<Reservation>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ParkingSpot>()
+            .HasIndex(p => p.Code)
+            .IsUnique();
+
+        modelBuilder.Entity<Reservation>()
+            .HasOne(r => r.ParkingSpot)
+            .WithMany(p => p.Reservations)
+            .HasForeignKey(r => r.ParkingSpotId);
+    }
+}
